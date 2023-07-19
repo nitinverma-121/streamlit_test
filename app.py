d5fb7f4e6cbea93a4a4de7530907c76587052771
nitin verma <nitinverma03241@gmail.com>
2023-07-19 22:29:51 +0530

    update commit

--- app.py
+++ app.py
@@ -22,20 +22,15 @@ def main():
     </div>
     """
     st.markdown(html_temp, unsafe_allow_html=True)
-    activities = ['Naive bayes']
-    option = st.sidebar.selectbox('Which model would you like to use?', activities)
+   
     st.text("")
     
     title = st.text_input('Write the statement',)
 
     a_ser = pd.Series(title)
     
-    st.subheader(option)
-
     if st.button('Classify'):
-        
-        if option == 'Naive bayes':
-            st.success(classify(clf.predict(a_ser)))
+        st.success(classify(clf.predict(a_ser)))
       
 if __name__ == '__main__':
     main()
